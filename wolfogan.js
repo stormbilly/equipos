74700a61742f35bb19df776b96cf944a4438bea9
Wolfogan <wolfogan@gmail.com>
2013-12-28 03:21:11 -0800

    Mis propósitos wolfogan
    Gracias Mejorando.la

---
+++ wolfogan.js
@@ -0,0 +1,12 @@
+## Propósito del año 2014:
+
+Hola creadores de la web:
+
+- El próximo año sere uno de los mejores backend en México, ayudaré a toda la comunidad que crea la web y   
+en especial a mejorando.la y haremos una web cada vez más divertida y revolucionaria.
+
+- Terminaré todos los cursos de Mejorando.la y colaboraré mas con mis compañeros activamente.
+
+- Colaboraré mas en la comunidad Mejorando.la.
+
+Edgar Iván Avila Garrido
