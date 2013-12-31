1e0198852342c5c374ba30e450dd6cbed8c21b48
José Aguirre <intercoresystems@gmail.com>
2013-12-30 22:40:09 -0700

    ANProposito.m

---
+++ ANProposito.m
@@ -0,0 +1,7 @@
+Mi proposito de año nuevo es ser un excelente desarrollador en tecnologias moviles nativas de iOS y Android.
+
+Tambien poder terminar la plataforma que tengo planeada liberar este año.
+
+Muchas gracias mejorandola.!!
+
+José de Jesús Aguirre Osuna.
